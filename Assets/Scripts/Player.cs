2989efad50e46b69a95949894e8ccce9787bedaa
aryankad1an <aryankaddian842@gmail.com>
2023-12-13 20:09:00 +0530

    better input manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player : MonoBehaviour
 {
     // for camera movement: use Cinemachine(really good plugin)
@@ -11,28 +10,12 @@ public class Player : MonoBehaviour
     // not just your editor but the other classes can access your variable which can be unsafe
     // using serialise field, this will be available to the unity editor
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
     private void Update()
     {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        } 
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = ( moveDir != new Vector3(0, 0, 0) ); // self explanatory
         
