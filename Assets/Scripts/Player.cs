7d7f3e6f70b5801d5104f5e6a1dc507ec3a4c487
aryankad1an <aryankaddian842@gmail.com>
2023-12-25 23:05:10 +0530

    improved collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,17 +19,56 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
         // adding collision detection
-        float playerSize = .7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize); // returns a boolean(hits or not)
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        //                                  lower point,                     upper point,                            radius of capsule,direction of cast,action distance                     
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up*playerHeight, playerRadius, moveDir, moveDistance); // returns a boolean(hits or not)
         // raycast is something like a laser and it tells if it hits something or not
+        // capsuleCase is raycast but fires a capsule size areal laser
+
         
         isWalking = ( moveDir != new Vector3(0, 0, 0) ); // self explanatory
 
         if (canMove)
         {
-            transform.position += moveDir * Time.deltaTime * moveSpeed; // transform refers to wherever the script is attached
+            transform.position += moveDir * moveDistance; // transform refers to wherever the script is attached
             // this time thingy will keep this uniform in different FPS 
 
+        }
+        else
+        {
+            // cannot move towards moveDir
+            /*
+             THIS CODE WILL HELP IN ACHIEVING DIAGONAL MOVEMENT
+             
+             imagine ur facing a wall(up direction) and ur trying to move in up and right direction, in this situation we will make player slowly move in right direction instead of completely disabling him from moving
+             */
+            
+            // Try X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            bool canMoveX = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up*playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMoveX)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Try Z Movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                bool canMoveZ = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up*playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMoveZ)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // cannot move in any direction(somehow)
+                    moveDir = Vector3.zero;
+                }
+            }
+
+            transform.position += moveDir * moveDistance;
         }
 
         // to add rotation with movement
